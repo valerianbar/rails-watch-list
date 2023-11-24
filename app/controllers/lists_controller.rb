e7f03f7bb3f2b6d0f00c52738887f562b60a71ae
valerian barreau <valerianextra@gmail.com>
2023-11-24 12:48:35 +0300

    watch-list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def index
+    @list = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+    @movies = @list.movies
+  end
+  def new
+    @list = List.new
+  end
+  def create
+
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+  def lists_params
+    params.require(:list).permit(:name)
+  end
+end
